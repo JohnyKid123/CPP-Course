7fdac3296223f12309b6c7ef8eef5c0b677d8d13
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-29 22:02:15 +0200

    Create program.cpp

---
+++ SU Course/Simple Calculations/Circle Area and Perimeter/program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    double a;
+    cin>>a;
+    double s = a*a*3.14159265358979323846;
+    double p = 2*(3.14159265358979323846)*a;
+    cout<<s<<endl;
+    cout<<p<<endl;
+
+    return 0;
+}
