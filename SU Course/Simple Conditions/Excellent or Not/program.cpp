976442c39eca5c74a2cee1f51fb6337545ccdacf
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2022-12-30 23:29:12 +0200

    Create program.cpp

---
+++ SU Course/Simple Conditions/Excellent or Not/program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a; 
+    cin>>a;
+    if(a>=5.50) cout<<"Excellent!"<<endl;
+  	else cout<<"Not excellent."<<endl;
+    return 0;
+}
