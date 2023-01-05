d96e38fbdc1b459dd5bd52067ddbd0c03a5923e7
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-05 22:10:48 +0200

    Create program.cs

---
+++ SU Course/Complex Conditions/Animal Type/program.cs
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string animal;
+    cin>>animal;
+        
+if (animal == "dog")
+{
+cout <<"mammal" << endl;
+}
+else if (animal == "crocodile" || animal == "tortoise" || animal == "snake") 
+{
+cout << "reptile" << endl;
+}
+else {
+cout << "unknown" << endl;
+}
+        
+    return 0;
+}
