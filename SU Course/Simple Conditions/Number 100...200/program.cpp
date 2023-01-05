bc7a09e45bf7c657c5e2b231ca4384b24ea3566f
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-05 22:57:30 +0200

    Create program.cpp

---
+++ SU Course/Simple Conditions/Number 100...200/program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+            int a;
+             cin>>a;
+             if(a<100) cout<<"Less than 100"<<" ";
+             else if(a>=100 && a<=200) cout<<"Between 100 and 200"<<" ";
+             else if(a>200) cout<<"Greater than 200"<<" ";
+    return 0;
+}
