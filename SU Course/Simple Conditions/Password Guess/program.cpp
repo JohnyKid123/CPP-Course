1e823c40ada49675bd4cce34eff21361a91ce1ae
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-05 22:57:07 +0200

    Create program.cpp

---
+++ SU Course/Simple Conditions/Password Guess/program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+            string a;
+             cin>>a;
+             if(a=="s3cr3t!P@ssw0rd") cout<<"Welcome"<<endl;
+             else cout<<"Wrong password!"<<endl;
+    return 0;
+}
