5951afaa9ef058710f4d10405347fb2ee886938a
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-06 23:59:16 +0200

    Create program.cpp

---
+++ SU Course/Simple Conditions/3 Equal Numbers/program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>  // include the string library
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b && b==c && a==c)
+    cout<<"yes"<<" ";
+    else cout<<"no"<<" ";
+
+    return 0;
+}
