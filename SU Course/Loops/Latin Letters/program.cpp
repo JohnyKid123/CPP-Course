bf04ffc0e6f9c9f4a45cf2e6b1159eada8529a3b
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-12 23:51:23 +0200

    Create program.cpp

---
+++ SU Course/Loops/Latin Letters/program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                cout<<letter<<" ";
+            }
+    return 0;
+}
