9ebfed55525262f02d4b4fd184917289ea89c1b2
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-12 23:51:04 +0200

    Create program.cpp

---
+++ SU Course/Loops/Numbers Ending in 7/program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int a = 1; a <= 1000; a++)
+            {
+                if (a % 10 == 7) cout<<a<<" ";
+            }
+    return 0;
+}
