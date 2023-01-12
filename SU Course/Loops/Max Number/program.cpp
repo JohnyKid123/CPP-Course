ca9d324ec9f1330cab3493ebe9fb821fb5331ab5
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-12 23:52:03 +0200

    Create program.cpp

---
+++ SU Course/Loops/Max Number/program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+            int n;
+            cin>>n;
+            long max = -1000000000000000;
+            for (int i = 1; i <= n; i++)
+            {
+                int num; cin>>num;
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            cout<<"max = "<<max<<" ";
+
+    return 0;
+}
