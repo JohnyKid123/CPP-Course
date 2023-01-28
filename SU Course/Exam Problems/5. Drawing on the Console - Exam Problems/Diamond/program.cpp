f5d8e8d74f0457d4648fde4aa1806cee3fbe8df2
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-28 21:45:54 +0200

    Create program.cpp

---
+++ SU Course/Exam Problems/5. Drawing on the Console - Exam Problems/Diamond/program.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+namespace Diamond
+{
+    int main()
+    {
+        int n;
+        cin >> n;
+
+        int rowsCount, innerDashes;
+        if (n % 2 == 0)
+        {
+            rowsCount = n - 1;
+            innerDashes = 0;
+        }
+        else
+        {
+            rowsCount = n;
+            innerDashes = -1;
+        }
+
+        int outerDashes = rowsCount / 2;
+
+        for (int i = 0; i < rowsCount; i++)
+        {
+            for (int j = 0; j < outerDashes; j++) cout << '-';
+
+            cout << '*';
+            for (int j = 0; j < innerDashes; j++) cout << '-';
+            if (innerDashes >= 0) cout << '*';
+
+            for (int j = 0; j < outerDashes; j++) cout << '-';
+
+            cout << endl;
+
+            if (i < rowsCount / 2)
+            {
+                outerDashes--;
+                innerDashes += 2;
+            }
+            else
+            {
+                outerDashes++;
+                innerDashes -= 2;
+            }
+        }
+        return 0;
+    }
+}
