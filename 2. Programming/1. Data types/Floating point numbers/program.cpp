62550368116ad93d864a506b1b08fe2fd525f3f0
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-30 22:37:31 +0200

    Create program.cpp

---
+++ 2. Programming/1. Data types/Floating point numbers/program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    double num, num1, num2;
+    std::cin >> num >> num1 >> num2;
+    std::cout << num << std::endl;
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    return 0;
+}
