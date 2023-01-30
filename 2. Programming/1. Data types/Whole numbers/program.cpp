3237b1384b5532febbf38058d3cd6e22049430a7
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-30 22:42:30 +0200

    Create program.cpp

---
+++ 2. Programming/1. Data types/Whole numbers/program.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+  int main()
+  {
+        short num = -100;
+        unsigned char num1 = 128;
+        short num2 = -3540;
+        int num3 = 64876;
+        long long num4 = 2147483648;
+        int num5 = -1141583228;
+        long long num6 = -1223372036854775808;
+        cout << num << endl;
+        cout << static_cast<int>(num1) << endl;
+        cout << num2 << endl;
+        cout << num3 << endl;
+        cout << num4 << endl;
+        cout << num5 << endl;
+        cout << num6 << endl;
+      return 0;
+  }
+
+
+
+
