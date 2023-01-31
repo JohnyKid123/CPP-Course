43b71ec6d3b5849137023614d025ec14277f1bed
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-31 23:49:54 +0200

    Create program.cpp

---
+++ 2. Programming/1. Data types/Boolean variable/program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+std::string input;
+std::cin >> input;
+if (input == "true")
+{
+    std::cout << "Yes" << std::endl;
+}
+else
+{
+    std::cout << "No" << std::endl;
+}
+
+return 0;
+}
