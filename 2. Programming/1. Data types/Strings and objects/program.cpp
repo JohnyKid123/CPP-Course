663828a74ec85d99205373d0bc788a31efabf8c3
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-02-16 21:25:15 +0200

    Create program.cpp

---
+++ 2. Programming/1. Data types/Strings and objects/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+
+    getline(cin, a);
+    getline(cin, b);
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
