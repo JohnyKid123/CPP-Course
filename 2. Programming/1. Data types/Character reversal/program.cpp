16411bfc0e95dbbb4674691d8fc432f1c3e3f936
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-02-16 21:21:24 +0200

    Create program.cpp

---
+++ 2. Programming/1. Data types/Character reversal/program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b, c;
+    getline(cin, a);
+    getline(cin, b);
+    getline(cin, c);
+    cout << c << b << a << endl;
+    return 0;
+}
